0da9b03bcfe2e2091a098df040936b4555d5493a
Henrique Magno <henrique.longatti2010@engenharia.ufjf.br>
2021-11-30 10:11:55 -0300

    plot das juntas do roboô

--- python/robo_articulado.py
+++ python/robo_articulado.py
@@ -1,10 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Artic_Robot():
-    def __init__(self,link1,link2,link3):
+    'Robô Articulado'
+    def __init__(self,link1,link2,link3,th1,th2,th3):
         self.link1 = link1
         self.link2 = link2
         self.link3 = link3
+        self.th1 = th1
+        self.th2 = th2
+        self.th3 = th3
 
     @property
     def first_joy(self):
@@ -22,10 +27,41 @@ class Artic_Robot():
             np.cos(th1)*(self.link3*np.cos(th2 + th3) + self.link2*np.cos(th2)),
             np.sin(th1)*(self.link3*np.cos(th2 + th3) + self.link2*np.cos(th2)),
             self.link1 + self.link3*np.sin(th2 + th3) + self.link2*np.sin(th2)
-
         ])
 
-robo = Artic_Robot(50,50,50)
-print(robo.first_joy)
-print(robo.second_joy(90,90))
-print(robo.third_joy(90,90,90))
+class Plot_Robo(Artic_Robot):
+
+    def plot(self):
+        j_1 = self.first_joy
+        j_2 = self.second_joy(self.th1,self.th2)
+        j_3 = self.third_joy(self.th1,self.th2,self.th3)
+        plt.rcParams['figure.figsize'] = (8,6)
+        ax = plt.axes(projection='3d');
+        ax.scatter3D([j_1[0],j_2[0],j_3[0]],
+                    [j_1[1],j_2[1],j_3[1]],
+                    [j_1[2],j_2[2],j_3[2]],s=1000); 
+    
+   
+        plt.show()
+
+robo = Plot_Robo(0.5,0.5,0.5,0,60,120)
+robo.plot()
+
+'''
+robo = Artic_Robot(0.05,0.05,0.05)
+j_1 = robo.first_joy
+j_2 = robo.second_joy(90,90)
+j_3 = robo.third_joy(90,90,90)
+
+print(j_1)
+print(j_2)
+print(j_3)
+
+plt.rcParams['figure.figsize'] = (8,6)
+ax = plt.axes(projection='3d');
+ax.scatter3D([j_1[0],j_2[0],j_3[0]],
+            [j_1[1],j_2[1],j_3[1]],
+            [j_1[2],j_2[2],j_3[2]]); 
+   
+plt.show() 
+'''
