b47107b3d78d2e12174ed6199016ad796de9f6d8
Henrique Magno <henrique.longatti2010@engenharia.ufjf.br>
2021-12-06 08:37:59 -0300

    plote do robo

--- python/robo_articulado.py
+++ python/robo_articulado.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import geometria_alalitica as ga
 
 class Artic_Robot():
     'Robô Articulado'
@@ -36,35 +38,24 @@ class Plot_Robo(Artic_Robot):
         j_2 = self.second_joy(self.th1,self.th2)
         j_3 = self.third_joy(self.th1,self.th2,self.th3)
         
-        #criar uma reta entre as juntas 
+        l1 = ga.reta([0,0,0],j_1)
+        l2 = ga.reta(j_1,j_2,50)
+        l3 = ga.reta(j_2,j_3,50)
 
         plt.rcParams['figure.figsize'] = (8,6)
         ax = plt.axes(projection='3d');
         ax.scatter3D([j_1[0],j_2[0],j_3[0]],
                     [j_1[1],j_2[1],j_3[1]],
                     [j_1[2],j_2[2],j_3[2]],s=1000); 
-    
+
+        ax.plot(l1[:,0],l1[:,1],l1[:,2])
+        ax.plot(l2[:,0],l2[:,1],l2[:,2])
+        ax.plot(l3[:,0],l3[:,1],l3[:,2])
    
         plt.show()
 
-robo = Plot_Robo(0.5,0.5,0.5,0,60,120)
+
+robo = Plot_Robo(0.5,0.5,0.5,30,0,0)
 robo.plot()
 
-'''
-robo = Artic_Robot(0.05,0.05,0.05)
-j_1 = robo.first_joy
-j_2 = robo.second_joy(90,90)
-j_3 = robo.third_joy(90,90,90)
 
-print(j_1)
-print(j_2)
-print(j_3)
-
-plt.rcParams['figure.figsize'] = (8,6)
-ax = plt.axes(projection='3d');
-ax.scatter3D([j_1[0],j_2[0],j_3[0]],
-            [j_1[1],j_2[1],j_3[1]],
-            [j_1[2],j_2[2],j_3[2]]); 
-   
-plt.show() 
-'''
